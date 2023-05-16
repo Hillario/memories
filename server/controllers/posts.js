a6c9305f410557687aceb478e2e9ef896c709aa4
Hillario <hillcheswiztyphoon@gmail.com>
2023-05-16 10:06:17 +0300

    completed handlers for routes and added material-ui

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,17 @@
+import postMessage from '../models/postMessage.js';
+
 //all the handlers for our routes
-export const getPosts = (req, res) => {
-    res.send('THIS WORKS!');
+export const getPosts = async (req, res) => {
+    try {
+        //this is an asynchronous action that's why we add await
+        const postMessages = await  postMessage.find();
+        console.log(postMessages);
+        res.status(200).json(postMessages); 
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createPosts = (req, res) => {
+    res.send('Post Creation');
 }
